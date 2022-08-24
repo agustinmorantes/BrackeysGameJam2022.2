42db32c527910078abe3d8355eb1a108cbe60b09
Agustín Morantes <agustinmorantes@gmail.com>
2022-08-24 15:52:45 -0300

    Health component (#9)
    * Remove unused packages
    
    * Config Fixes
    
    * Added health component

---
+++ Assets/BrackeysGameJam/Scripts/Health.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace BrackeysGameJam
+{
+    public class Health : MonoBehaviour
+    {
+        public float maxHP = 100.0f;
+        public UnityEvent died;
+        public UnityEvent healthChanged;
+        public UnityEvent damaged;
+        public UnityEvent healed;
+        
+        private float _currentHP;
+        
+        public float CurrentHP => _currentHP;
+        public bool IsDead => _currentHP <= 0;
+
+        private void Start()
+        {
+            _currentHP = maxHP;
+            healthChanged.Invoke();
+        }
+
+        public void Damage(float amount)
+        {
+            if (IsDead) return;
+            
+            _currentHP = Mathf.Clamp(_currentHP - amount, 0, maxHP);
+            healthChanged.Invoke();
+            damaged.Invoke();
+            
+            if (IsDead) died.Invoke();
+        }
+
+        public void Heal(float amount)
+        {
+            _currentHP = Mathf.Clamp(_currentHP + amount, 0, maxHP);
+            healthChanged.Invoke();
+            healed.Invoke();
+        }
+    }
+}
